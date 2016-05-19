71f881eafa8d7b8c765b787fad7d8ea4453804aa
KNIGHTMASTER <fauzi.knigtmaster.achmad@gmail.com>
2016-05-19 11:36:34 +0700

    Initial Commit

---
+++ src/main/java/com/zisal/learn/vaadin/DCGeneratorApplication.java
@@ -0,0 +1,19 @@
+package com.zisal.learn.vaadin;
+
+import com.vaadin.spring.annotation.EnableVaadin;
+import com.zisal.learn.vaadin.constant.ApplicationConstant.Configuration.SpringConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@ComponentScan(SpringConfiguration.COMPONENT_SCAN)
+@SpringBootApplication
+@EnableVaadin
+public class DCGeneratorApplication extends ServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DCGeneratorApplication.class, args);
+	}
+}
---
+++ src/main/java/com/zisal/learn/vaadin/constant/ApplicationConstant.java
@@ -0,0 +1,34 @@
+package com.zisal.learn.vaadin.constant;
+
+/**
+ * Created by Ladies Man on 4/21/2016.
+ */
+public interface ApplicationConstant {
+
+    interface VAADINTheme {
+        String VALO             = "valo";
+    }
+
+    interface View{
+        String THEME            = VAADINTheme.VALO;
+        interface ListOfValue {
+            String CAPTION_PROPERTY = "data";
+            interface DisplayDataRowTable{
+                String ITEM_5       = "5";
+                String ITEM_10      = "10";
+                String ITEM_15      = "15";
+                String ITEM_20      = "20";
+                String ITEM_50      = "50";
+                float WIDTH         = 80;
+            }
+        }
+    }
+
+    interface Configuration{
+        interface SpringConfiguration{
+            String COMPONENT_SCAN   = "com.zisal.learn.vaadin";
+        }
+        String RESOURCE_BUNDLE_BASE_NAME    = "classpath:locale/message";
+        int RB_CACHE_SECONDS    = 3600;
+    }
+}
